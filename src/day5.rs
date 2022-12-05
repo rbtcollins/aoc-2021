11593e596c60ac56dfa3e7c500ca93a512b975a3
Robert Collins <robertc@robertcollins.net>
2022-12-05 07:27:54 +0100

    Day 5

--- src/day5.rs
+++ src/day5.rs
@@ -1,152 +1,120 @@
 use std::str;
 
 use aoc_runner_derive::{aoc, aoc_generator};
-use itertools::Itertools;
 
 #[derive(Default, Clone, Debug)]
-struct Point {
-    x: usize,
-    y: usize,
+struct Move {
+    number: usize,
+    from: usize,
+    to: usize,
 }
 
 #[derive(Default, Clone, Debug)]
-struct Line {
-    start: Point,
-    end: Point,
-}
-
-#[derive(Default, Clone, Debug)]
-struct Floor {
-    floor: Vec<Vec<usize>>,
-}
-
-#[derive(Default, Clone, Debug)]
-struct Game {
-    lines: Vec<Line>,
+struct Plan {
+    stacks: Vec<Vec<char>>,
+    moves: Vec<Move>,
 }
 
 #[aoc_generator(day5)]
-fn generate(input: &str) -> Game {
-    Game {
-        lines: input
-            .lines()
-            .flat_map(|s| s.split(" -> "))
-            .flat_map(|s| s.split(','))
-            .flat_map(|s| s.parse())
-            .chunks(2)
-            .into_iter()
-            .map(|mut scalars| Point {
-                x: scalars.next().unwrap(),
-                y: scalars.next().unwrap(),
-            })
-            .chunks(2)
-            .into_iter()
-            .map(|mut points| Line {
-                start: points.next().unwrap(),
-                end: points.next().unwrap(),
-            })
-            .collect(),
-    }
-}
-
-impl Line {
-    fn axis_aligned(&self) -> bool {
-        self.start.x == self.end.x || self.start.y == self.end.y
-    }
-}
-
-impl Floor {
-    fn size_from<'a, V: Iterator<Item = &'a Line>>(vents: V) -> usize {
-        let mut max = 0;
-        for line in vents {
-            max = std::cmp::max(max, line.start.x);
-            max = std::cmp::max(max, line.start.y);
-            max = std::cmp::max(max, line.end.x);
-            max = std::cmp::max(max, line.end.y);
-        }
-        max + 1 // index 9 => 10 positions
-    }
-
-    fn new(size: usize) -> Self {
-        let mut res = Floor::default();
-        for _ in 0..size {
-            let row = vec![0; size];
-            res.floor.push(row);
-        }
-        res
-    }
-
-    fn render<'a, V: Iterator<Item = &'a Line>>(&mut self, vents: V) {
-        for line in vents {
-            // 45' diagonal
-            // 3,1 -> -2
-            let width_x: isize = line.end.x as isize - line.start.x as isize;
-            // -2 -> 2
-            let steps_x = width_x.abs();
-            // -2/2 = -1
-            let delta_x = if steps_x == 0 { 0 } else { width_x / steps_x };
-            let width_y: isize = line.end.y as isize - line.start.y as isize;
-            let steps_y = width_y.abs();
-            let delta_y = if steps_y == 0 { 0 } else { width_y / steps_y };
-            let steps = if steps_x == 0 { steps_y } else { steps_x };
-            let mut x = line.start.x;
-            let mut y = line.start.y;
-            for _ in 0..steps + 1 {
-                *self.cell(x, y) += 1;
-                x = (x as isize + delta_x) as usize;
-                y = (y as isize + delta_y) as usize;
+fn generate(input: &str) -> Plan {
+    let mut input = input.lines();
+    let mut stacks_matrix = input
+        .by_ref()
+        .take_while(|l| !l.starts_with(" 1"))
+        .map(|l| {
+            l.as_bytes()
+                .chunks(4)
+                .map(|c| {
+                    if c[1] == b' ' {
+                        None
+                    } else {
+                        Some(char::from(c[1]))
+                    }
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    stacks_matrix.reverse();
+    let len = stacks_matrix[0].len();
+    let stacks = (-1..len as i8)
+        .into_iter()
+        .map(|i| {
+            if i >= 0 {
+                // a single empty stack at offset 0.
+                stacks_matrix
+                    .iter()
+                    .filter_map(|row| row[i as usize])
+                    .collect()
+            } else {
+                vec![]
             }
-        }
-    }
+        })
+        .collect::<Vec<Vec<_>>>();
+    // skip the index line
+    input.next();
+    // create moves vector
+    // move 6 from 6 to 5
+    let moves = input
+        .filter(|l| !l.is_empty())
+        .map(|l| {
+            let l = l.split(' ').collect::<Vec<_>>();
+            Move {
+                number: l[1].parse().unwrap(),
+                from: l[3].parse().unwrap(),
+                to: l[5].parse().unwrap(),
+            }
+        })
+        .collect::<Vec<_>>();
 
-    fn cell(&mut self, x: usize, y: usize) -> &mut usize {
-        &mut self.floor[x][y]
-    }
-
-    fn cells(&self) -> impl Iterator<Item = usize> + '_ {
-        self.floor.iter().flat_map(|r| r.iter()).copied()
-    }
+    Plan { stacks, moves }
 }
 
 #[aoc(day5, part1)]
-fn part1(input: &Game) -> usize {
-    let (vents1, vents2) = input.lines.iter().filter(|l| l.axis_aligned()).tee();
-    let mut floor = Floor::new(Floor::size_from(vents1));
-    floor.render(vents2);
-    floor.cells().filter(|cell| cell >> 1 > 0).count()
+fn part1(input: &Plan) -> String {
+    let mut stacks = input.stacks.clone();
+    for crate_move in &input.moves {
+        for _ in 0..crate_move.number {
+            let item = stacks[crate_move.from].pop().unwrap();
+            stacks[crate_move.to].push(item);
+        }
+    }
+    stacks.iter().skip(1).map(|s| s.last().unwrap()).collect()
 }
 
 #[aoc(day5, part2)]
-fn part2(input: &Game) -> usize {
-    let mut floor = Floor::new(Floor::size_from(input.lines.iter()));
-    floor.render(input.lines.iter());
-    floor.cells().filter(|cell| cell >> 1 > 0).count()
+fn part2(input: &Plan) -> String {
+    let mut stacks = input.stacks.clone();
+    for crate_move in &input.moves {
+        let from = &mut stacks[crate_move.from];
+        let mut items = from.split_off(from.len() - crate_move.number);
+        stacks[crate_move.to].append(&mut items);
+    }
+    stacks.iter().skip(1).map(|s| s.last().unwrap()).collect()
 }
 
 #[cfg(test)]
 mod tests {
 
-    const SAMPLE: &str = r#"0,9 -> 5,9
-8,0 -> 0,8
-9,4 -> 3,4
-2,2 -> 2,1
-7,0 -> 7,4
-6,4 -> 2,0
-0,9 -> 2,9
-3,4 -> 1,4
-0,0 -> 8,8
-5,5 -> 8,2
+    const SAMPLE: &str = r#"    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
 "#;
 
     #[test]
     fn part1() {
         let input = super::generate(SAMPLE);
-        assert_eq!(5, super::part1(&input));
+        assert_eq!("CMZ", super::part1(&input));
     }
 
     #[test]
     fn part2() {
         let input = super::generate(SAMPLE);
-        assert_eq!(12, super::part2(&input));
+        assert_eq!("MCD", super::part2(&input));
     }
 }
