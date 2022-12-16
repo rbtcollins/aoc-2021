c4b29a49d19a2fa9ff31e991e95a363903d25b6f
Robert Collins <robertc@robertcollins.net>
2022-12-16 19:48:00 +0100

    day 13

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod day10;
 mod day11;
 mod day12;
+mod day13;
 
 aoc_main::main! {
     year 2022;
     day10 : generate => part_1, part_2;
     day11 : generate => part_1, part_2;
     day12 : generate => part_1, part_2;
+    day13 : generate => part_1, part_2;
 }
