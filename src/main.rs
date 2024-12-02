5260c8d75ef6e2a8e6641317fb6cb318c7544b7c
Robert Collins <robertc@robertcollins.net>
2024-12-02 14:48:37 +0100

    day1 2024. See the branch 2023 for older years.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,6 @@
-mod day10;
-mod day11;
-mod day12;
-mod day13;
-mod day14;
-mod day15;
-mod day16;
-mod day17;
+mod day1;
 
 aoc_main::main! {
-    year 2022;
-    day10 : generate => part_1, part_2;
-    day11 : generate => part_1, part_2;
-    day12 : generate => part_1, part_2;
-    day13 : generate => part_1, part_2;
-    day14 : generate => part_1, part_2;
-    day15 : generate => part_1, part_2;
-    day16 : generate => part_1, part_2;
-    day17 : generate => part_1;
+    year 2024;
+    day1 : generate => part_1, part_2;
 }
