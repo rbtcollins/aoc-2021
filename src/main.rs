c5742a981a91db98fb2aff4bbdb7141cfa3c8f87
Robert Collins <robertc@robertcollins.net>
2024-12-08 20:58:41 +0100

    2024 day 8

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod day4;
 mod day5;
 mod day6;
 mod day7;
+mod day8;
 
 aoc_main::main! {
     year 2024;
@@ -15,4 +16,5 @@ aoc_main::main! {
     day5 : generate => part_1, part_2, part_1_rayon, part_2_rayon;
     day6 : generate => part_1, part_2;
     day7 : generate => part_1, part_1_from_end, part_2, part_2_from_end;
+    day8 : generate => part_1, part_2;
 }
